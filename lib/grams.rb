3697392c81fa26d90a0601db96d3800f534ee304
Ilene Gorski <ilene.gorski@gmail.com>
2017-04-21 11:02:32 -0700

    add messages for anagrams, palindromes, and nothing special

--- lib/grams.rb
+++ lib/grams.rb
@@ -14,36 +14,13 @@ class String
     anagrams = obj_hash == arg_hash
     palindromes = obj_array.reverse() == arg_array
     if anagrams
-      "These words are anagrams"
       if palindromes
-        "These words are also palindromes"
+        "These words are not only anagrams but also palindromes"
+      else
+        "These words are anagrams"
       end
     else
       "These words are nothing special"
+    end
   end
-
-  # define_method(:grams) do |comparison|
-  #   obj_hash = Hash.new()
-  #   arg_hash = Hash.new()
-  #   obj_array = self.split("")
-  #   arg_array = comparison.split("")
-  #   obj_array.each() do |letter|
-  #     if obj_hash.has_key?(letter) == false
-  #       obj_hash.store(letter, 0)
-  #     elsif obj_hash.has_key?(letter) == true
-  #      # not really sure what to do here...
-  #     end
-  #   end
-  #   arg_array.each() do |letter|
-  #     arg_hash.store(letter, 0)
-  #   end
-  #   obj_hash, arg_hash
-  # end
-
-#this is how to add the numbers for each key
-#   myhash = Hash.new(0)
-# ['a', 'b', 'c', 'c'].each() do |num|
-#   myhash[num] += 1
-# end
-# myhash
 end
--- spec/grams_spec.rb
+++ spec/grams_spec.rb
@@ -3,28 +3,17 @@ require ("grams")
 require ("pry")
 
 describe('String#grams') do
-  # it('returns a list of letters') do
-  #   expect("hi".grams("bye")).to(eq([['h', 'i'], ['b', 'y', 'e']]))
-  # end
-  # it('creates a hash for both words') do
-  #   expect("hi".grams("bye")).to(eq([{"h"=>1, "i"=>1}, {"b"=>1, "y"=>1, "e"=>1}]))
-  # end
-  # it('stores correct number of letters in words') do
-  #   expect("boo".grams("soon")).to(eq([{"b"=>1, "o"=>2}, {"s"=>1, "o"=>2, "n"=>1}]))
-  # end
-  # it('disregards case') do
-  #   expect("Boo".grams("sOon")).to(eq([{"b"=>1, "o"=>2}, {"s"=>1, "o"=>2, "n"=>1}]))
-  # end
-  # it('returns false if words are not anagrams') do
-  #   expect("hello".grams("goodbye")).to(eq(false, false))
-  # end
-  # it('returns true if words are anagrams') do
-  #   expect("stick".grams("ticks")).to(eq(true, false))
-  # end
-  # it('returns true if words are anagrams') do
-  #   expect("shuttle".grams("ttulesh")).to(eq(true, false))
-  # end
-  it('returns true if words are palindromes') do
-    expect("mined".grams("denim")).to(eq( true))
+
+  it('gives disappointing message when words are not anagrams') do
+    expect("Boo".grams("sOon")).to(eq("These words are nothing special"))
   end
+
+  it('confirms suspicions if words are anagrams') do
+    expect("stick".grams("ticks")).to(eq("These words are anagrams"))
+  end
+
+  it('delivers joyous message upon receiving a palindrome') do
+    expect("mined".grams("denim")).to(eq("These words are not only anagrams but also palindromes"))
+  end
+
 end
