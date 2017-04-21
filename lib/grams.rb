71660f2eb21e2fc97afa5a39e43783813c71436f
Ilene Gorski <ilene.gorski@gmail.com>
2017-04-21 11:28:36 -0700

    add antigram logic

--- lib/grams.rb
+++ lib/grams.rb
@@ -1,29 +1,42 @@
 class String
   define_method(:grams) do |comparison|
+    vowels = ['a','e','i','o','u','y']
+
     obj = self.delete(" ")
     arg = comparison.delete(" ")
 
     obj_array = obj.downcase.split("")
     arg_array = arg.downcase.split("")
 
-    obj_hash = Hash.new(0)
-    obj_array.each() do |letter|
-      obj_hash[letter] += 1
-    end
-    arg_hash = Hash.new(0)
-    arg_array.each() do |letter|
-      arg_hash[letter] += 1
-    end
-    anagrams = obj_hash == arg_hash
-    palindromes = obj_array.reverse() == arg_array
-    if anagrams
-      if palindromes
-        "These words are not only anagrams but also palindromes"
-      else
-        "These words are anagrams"
-      end
+    shared_obj = vowels & obj_array.to_a
+    shared_arg = vowels & arg_array.to_a
+
+    if shared_obj == [] || shared_arg == []
+      "Hey... that's not even a word! Try again."
     else
-      "These words are nothing special"
+      if obj_array & arg_array.to_a == []
+        "No letters match! You have an antigram."
+      else
+        obj_hash = Hash.new(0)
+        obj_array.each() do |letter|
+          obj_hash[letter] += 1
+        end
+        arg_hash = Hash.new(0)
+        arg_array.each() do |letter|
+          arg_hash[letter] += 1
+        end
+        anagrams = obj_hash == arg_hash
+        palindromes = obj_array.reverse() == arg_array
+        if anagrams
+          if palindromes
+            "These words are not only anagrams but also palindromes."
+          else
+            "These words are anagrams."
+          end
+        else
+          "These words are nothing special."
+        end
+      end
     end
   end
 end
--- spec/grams_spec.rb
+++ spec/grams_spec.rb
@@ -5,17 +5,23 @@ require ("pry")
 describe('String#grams') do
 
   it('gives disappointing message when words are not anagrams') do
-    expect("Boo".grams("sOon")).to(eq("These words are nothing special"))
+    expect("Boo".grams("sOon")).to(eq("These words are nothing special."))
   end
 
   it('confirms suspicions if words are anagrams') do
-    expect("stick".grams("ticks")).to(eq("These words are anagrams"))
+    expect("stick".grams("ticks")).to(eq("These words are anagrams."))
   end
 
   it('delivers joyous message upon receiving a palindrome') do
-    expect("mined".grams("denim")).to(eq("These words are not only anagrams but also palindromes"))
+    expect("mined".grams("denim")).to(eq("These words are not only anagrams but also palindromes."))
   end
   it('ignores spaces') do
-    expect("m ine d".grams("denim")).to(eq("These words are not only anagrams but also palindromes"))
+    expect("m ine d".grams("denim")).to(eq("These words are not only anagrams but also palindromes."))
+  end
+  it('does not allow words without vowels') do
+    expect("mdskj".grams("feel")).to(eq("Hey... that's not even a word! Try again."))
+  end
+  it('checks for antigrams') do
+    expect("hi".grams("bye")).to(eq("No letters match! You have an antigram."))
   end
 end
